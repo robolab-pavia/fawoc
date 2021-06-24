03a6d97e68a81bb04f2a4a6218f811ba56e10a92
Guido Benetti <guido.benetti01@ateneopv.it>
2021-06-24 17:55:45 +0200

    Collect choices values in argparse custom class

--- fawoc/argumentParser.py
+++ fawoc/argumentParser.py
@@ -95,6 +95,7 @@ class ArgParse(argparse.ArgumentParser):
                 'non-standard': non_standard,
                 'required': ret.required,
                 'dest': ret.dest,
+                'choices': ret.choices,
                 'logfile': log,
                 'suggest-suffix': suggest,
                 'cli_only': cli_only,
